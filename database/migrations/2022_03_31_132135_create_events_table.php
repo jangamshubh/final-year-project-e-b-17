d4d468266b4f5051a4566084771c4f1c3b5682a3
Shubh Jangam <shubh.jangam@chitran.in>
2022-03-31 13:47:27 +0530

    Event Module Setup

---
+++ database/migrations/2022_03_31_132135_create_events_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->text('description')->nullable();
+            $table->text('type')->nullable();
+            $table->text('date')->nullable();
+            $table->text('no_of_people_expected')->nullable();
+            $table->text('start_time')->nullable();
+            $table->text('end_time')->nullable();
+            $table->foreignId('committee_id')->constrained('committees');
+            $table->foreignId('event_location_id')->constrained('event_locations');
+            $table->integer('is_approved')->default(0);
+            $table->integer('registration_approval_needed')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
